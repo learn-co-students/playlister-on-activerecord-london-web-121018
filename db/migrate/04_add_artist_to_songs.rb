255955fd25af585472bff204b32ea63881d6f6ea
github username <github email address>
2019-01-08 11:03:34 +0000

    Done.

---
+++ db/migrate/04_add_artist_to_songs.rb
@@ -0,0 +1,8 @@
+class AddArtistToSongs < ActiveRecord::Migration[5.2]
+
+  def change
+    add_column :songs, :artist_id, :integer
+  end
+
+
+end
---
+++ db/migrate/05_add_genre_to_songs.rb
@@ -0,0 +1,8 @@
+class AddGenreToSongs < ActiveRecord::Migration[5.2]
+
+  def change
+    add_column :songs, :genre_id, :integer
+  end
+
+
+end
